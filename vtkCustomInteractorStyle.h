ec01154139d0da983a527f1b18c8da0065cc50f1
Christian Gaser <christian.gaser@uni-jena.de>
2024-04-12 16:44:14 +0100

    Update vtkCustomInteractorStyle.h
    Added backflipping and resetting to InteractorStyle

--- vtkCustomInteractorStyle.h
+++ vtkCustomInteractorStyle.h
@@ -35,8 +35,10 @@ public:
       case 'w' :    case 'W' :
       case 'm' :    case 'M' :
       case 'f' :    case 'F' :
+      case '3' :
         vtkInteractorStyle::OnChar();
         break;
+      // Rotate upwards
       case 'u' :    case 'U' :
       {
         vtkCamera *camera = this->CurrentRenderer->GetActiveCamera();
@@ -47,6 +49,7 @@ public:
         rwi->Render();
         break;
       }
+      // Rotate downwards
       case 'd' :    case 'D' :
       {
         vtkCamera *camera = this->CurrentRenderer->GetActiveCamera();
@@ -57,6 +60,7 @@ public:
         rwi->Render();
         break;
       }
+      // Rotate left
       case 'l' :    case 'L' :
       {
         vtkCamera *camera = this->CurrentRenderer->GetActiveCamera();
@@ -67,6 +71,7 @@ public:
         rwi->Render();
         break;
       }
+      // Rotate right
       case 'r' :    case 'R' :
       {
         vtkCamera *camera = this->CurrentRenderer->GetActiveCamera();
@@ -77,6 +82,54 @@ public:
         rwi->Render();
         break;
       }
+      // Reset to original view
+      case 'o' :    case 'O' :
+      {
+        vtkCamera *camera = this->CurrentRenderer->GetActiveCamera();
+        this->CurrentRenderer->ResetCamera();
+        camera->OrthogonalizeViewUp();
+        camera->Zoom(2.0);
+        rwi->Render();
+        break;
+      }
+      // Flip top views backward to bottom views
+      case 'b' :
+      {
+        vtkCamera *camera = this->CurrentRenderer->GetActiveCamera();
+        vtkActorCollection* actors = this->CurrentRenderer->GetActors();
+
+        actors->InitTraversal();
+        vtkActor* actor;
+
+        // Estimate number of actors
+        int nActors = 0;
+        while ((actor = actors->GetNextActor()))
+          nActors++;
+        
+        // Reset
+        actors->InitTraversal();
+        int k = 0;
+        while ((actor = actors->GetNextActor())) {
+          k++;
+          vtkMapper* mapper = actor->GetMapper();
+          
+          // If overlay is used we have 12 actors and have to rotate the 2 meshes
+          // and the 2 overlay
+          if (nActors == 12) {
+            if ((k == 9) || (k == 10) || (k == 11) || (k == 12))
+              actor->RotateX(180);
+          } else {
+          // Otherwise just rotate the 2 meshes if no overlay is given
+            if ((k == 5) || (k == 6))
+              actor->RotateX(180);
+          }
+        }
+        
+        camera->OrthogonalizeViewUp();
+        rwi->Render();
+        break;
+      }
+      // Grab image and save as png file
       case 'g' :
       {
         string strTmp = rwi->GetRenderWindow()->GetWindowName();
@@ -108,12 +161,22 @@ public:
         << "     Rotate up/down/left/right by 1 degree." << endl
         << "  Ctrl/Cmd + u d l r" << endl
         << "     Rotate up/down/left/right by 180 degree." << endl
+        << "  b " << endl
+        << "     Flip top view backwards to bottom view." << endl
+        << "  o " << endl
+        << "     Reset to original view." << endl
         << "  w " << endl
         << "     Show wireframe." << endl
         << "  s " << endl
         << "     Show shaded." << endl
         << "  g " << endl
         << "     Grab image to png-file." << endl
+        << "  3 " << endl
+        << "     Stereo view" << endl
+        << "  f " << endl
+        << "     Zoom to the selected point." << endl
+        << "  p " << endl
+        << "     Select render and show its box." << endl
         << "  q e" << endl
         << "     Quit." << endl
         << endl
